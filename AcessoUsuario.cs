8d2d0e351126121a841acc230c6d12abe73865ba
Evandru <evandro_remolli.t@outlook.com>
2023-01-26 20:51:59 -0300

    First Commit

---
+++ AcessoUsuario.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginComSQLServer
+{
+    public static class AcessoUsuario
+    {
+        public static void Interface(SqlConnection Connection)
+        {
+            Console.Clear();
+            Console.WriteLine("Você deseja se registrar ou entrar no Sistema?");
+            Console.WriteLine();
+            Console.WriteLine("Escreva sua escolha:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Entrar / Registrar");
+            Console.ResetColor();
+            Console.Write("> ");
+            string escolha = Console.ReadLine();
+
+            if (escolha.ToLower() == "entrar")
+            {
+                InterfaceEntrar(Connection);
+                Thread.Sleep(3000);
+                Console.Clear();
+                Interface(Connection);
+            }
+            else if (escolha.ToLower() == "registrar")
+            {
+                InterfaceRegistro(Connection);
+                Thread.Sleep(3000);
+                Console.Clear();
+                Interface(Connection);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Escreva Corretamente!");
+                Console.ResetColor();
+            }
+        }
+        public static void InterfaceRegistro(SqlConnection Connection)
+        {
+            try
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Registrar Conta");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("Usuario: ");
+                string usuario = Console.ReadLine();
+                Console.Write("Senha: ");
+                string senha = Console.ReadLine();
+
+                if (usuario == "" || senha == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Usuário e Senha Inválidos!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Registrar(usuario, senha, Connection);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Erro ao registrar conta.");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void Registrar(string usuario, string senha, SqlConnection Connection)
+        {
+            string str = 
+                "SELECT * FROM Contas WHERE Usuario = '" + usuario + "'AND Senha = '" + senha + "'";
+            SqlCommand autentifica = new SqlCommand(str, Connection);
+            SqlDataReader Reader = autentifica.ExecuteReader();
+            if (Reader.Read() == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Esse usuário já existe");
+                Console.ResetColor();
+                Reader.Close();
+            }
+            else
+            {
+                try
+                {
+                    Reader.Close();
+                    string insert = 
+                        "INSERT INTO Contas VALUES('" + usuario + "', '" + senha + "')";
+                    SqlCommand registrar = new SqlCommand(insert, Connection);
+                    registrar.ExecuteNonQuery();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Conta Registrada!");
+                    Console.ResetColor();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Não foi possível registrar a conta.");
+                    Console.ResetColor();
+                    //Console.WriteLine(ex.Message);
+                    //Console.WriteLine(ex.StackTrace);
+                    
+                }
+            }
+        }
+        public static void InterfaceEntrar(SqlConnection Connection)
+        {
+            try
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Entrar no Sistema");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("Usuario: ");
+                string usuario = Console.ReadLine();
+                Console.Write("Senha: ");
+                string senha = Console.ReadLine();
+
+                if (usuario == "" || senha == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Usuário e Senha Inválidos!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Entrar(usuario, senha, Connection);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Erro ao entrar no sistema.");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void Entrar(string usuario, string senha, SqlConnection Connection)
+        {
+            try
+            {
+                string str = 
+                    "SELECT * FROM Contas WHERE Usuario = '" + usuario + "'AND Senha = '" + senha + "'";
+                SqlCommand entrar = new SqlCommand(str, Connection);
+                SqlDataReader Reader = entrar.ExecuteReader();
+                if (Reader.Read())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Acesso Concedido - Login efetuado");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Usuário e senha incorretos.");
+                    Console.ResetColor();
+                }
+                Reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Erro ao entrar!");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+
+            }
+        }
+        public static void CriaTabelaContas(SqlConnection Connection)
+        {
+            try
+            {
+                Conexao.UseDatabase(Connection);
+                string str = 
+                    "CREATE TABLE Contas(" +
+                    "ID int IDENTITY(1,1)," +
+                    "Usuario varchar(15) NOT NULL," +
+                    "Senha varchar(15) NOT NULL)";
+                SqlCommand comando = new SqlCommand(str, Connection);
+                comando.ExecuteNonQuery();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Tabela Contas criada.");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Tabela Contas já existe");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ Conexao.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginComSQLServer
+{
+    public static class Conexao
+    {
+        public static void Run()
+        {
+            SqlConnection Connection = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master");
+
+            try
+            {
+                //Conecta ao Banco de Dados master local
+                ConectarDB(Connection);
+
+                // Verifica existência do banco de dados local, se não existir, será criado
+                TentaCriarDB(Connection);
+
+                // Cria tabela Contas com as colunas ID - Usuario - Senha
+                AcessoUsuario.CriaTabelaContas(Connection);
+
+                PerguntaResetarDB(Connection);
+
+                //Executar a Interface até o usuário se registrar ou entrar no sistema
+                while (true)
+                {
+                    AcessoUsuario.Interface(Connection);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Erro ao conectar no Banco de Dados!");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                DesconectarDB(Connection);
+            }
+        }
+
+        //
+        //  ----------- METODOS SIMPLIFICADOS -----------
+        //
+
+        public static void ConectarDB(SqlConnection Connection)
+        {
+            Connection.Open();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Banco de Dados Conectado");
+            Console.ResetColor();
+        }
+        public static void DesconectarDB(SqlConnection Connection)
+        {
+            if (Connection.State == ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+        }
+        public static void ResetarDB(SqlConnection Connection)
+        {
+            try
+            {
+                UseDatabase(Connection);
+                string strDropDB = 
+                    "DROP TABLE Contas";
+                SqlCommand dropdb = new SqlCommand(strDropDB, Connection);
+                dropdb.ExecuteNonQuery();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("DATABASE RESETADA");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Erro no 'DropDB'");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void PerguntaResetarDB(SqlConnection Connection)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Você deseja resetar o Banco de Dados?");
+            Console.WriteLine("Sim / Nao");
+            Console.Write("> ");
+            string resetar = Console.ReadLine();
+            if (resetar.ToLower() == "sim")
+            {
+                ResetarDB(Connection);
+                AcessoUsuario.CriaTabelaContas(Connection);
+            }
+        }
+        public static void TentaCriarDB(SqlConnection Connection)
+        {
+            try
+            {
+                CriaDB(Connection);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Banco de Dados Criado.");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Banco de Dados já existe.");
+                Console.ResetColor();
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void CriaDB(SqlConnection Connection)
+        {
+            string databasePath = DatabasePath();
+            string strCriaDB;
+            strCriaDB = "CREATE DATABASE MyDatabase ON PRIMARY " +
+            "(NAME = MyDatabase, " +
+            "FILENAME = '" + databasePath + "MyDatabase.mdf', " +
+            "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%)" +
+            "LOG ON (NAME = MyDatabaseLog, " +
+            "FILENAME = '" + databasePath + "MyDatabaseLog.ldf', " +
+            "SIZE = 1MB, " +
+            "MAXSIZE = 5MB, " +
+            "FILEGROWTH = 10%)";
+
+            SqlCommand CriaDB = new SqlCommand(strCriaDB, Connection);
+            CriaDB.ExecuteNonQuery();
+        }
+        public static void UseDatabase(SqlConnection Connection)
+        {
+            // Executa o comando 'USE MyDatabase' para manipular dados dentro do Banco de Dados
+            string strUse = "USE MyDatabase;";
+            SqlCommand use = new SqlCommand(strUse, Connection);
+            use.ExecuteNonQuery();
+        }
+        public static string DatabasePath()
+        {
+            var atualPath = AppDomain.CurrentDomain.BaseDirectory;
+            string programPath = "";
+            for (int i = atualPath.Length - 1; i > atualPath.Length - 19; i--)
+            {
+                programPath = atualPath.Remove(i);
+            }
+            var path = $"{programPath}\\Database\\";
+            return path;
+        }
+    }
+}
